0b3936e41df55c1bb1df2aa5f45cf2dcf7203d98
thanco002 <codingminecrafter@gmail.com>
2025-03-03 16:54:18 -0500

    Merge from jetson nano changes

--- src/nk_vision_pkg/launch/nk_vision.launch.py
+++ src/nk_vision_pkg/launch/nk_vision.launch.py
@@ -83,14 +83,14 @@ def generate_launch_description():
                 get_package_share_directory('nk_vision') + '/launch/aruco_tracker_2.launch.xml')),
 
         IncludeLaunchDescription(AnyLaunchDescriptionSource(
-                get_package_share_directory('frc_2024_field_description') + '/launch/main.launch.py')),
+                get_package_share_directory('frc_2025_field_description') + '/launch/main.launch.py')),
         
         IncludeLaunchDescription(AnyLaunchDescriptionSource(
-                get_package_share_directory('robot_2024_description') + '/launch/main.launch.py')),
+                get_package_share_directory('robot_2025_description') + '/launch/main.launch.py')),
 
         launch_ros.actions.Node(
             package='nk_vision', executable='tf2network_table.py', output='screen',
-            parameters=[{'transfer_topics': ["world:base_link_1", "world:base_link_2", "world:base_link_3", "base_link:note"]}]),
+            parameters=[{'transfer_topics': ["world:base_link_1", "world:base_link_2", "world:base_link_3", "base_link:branch"]}]),
         
         launch_ros.actions.Node(
             package='nk_vision', executable='network_table2tf.py', output='screen',
@@ -109,11 +109,11 @@ def generate_launch_description():
                   'vision_logs/data'],
             output='screen'
         ),
-        IncludeLaunchDescription(PythonLaunchDescriptionSource(
-                get_package_share_directory('icp_obj_pose') + '/launch/component.launch.py'),
-                launch_arguments=[
-                           ('reference_ply_filepath', get_package_share_directory('nk_vision')+'/config/note.ply'),
-                           ('object_name', 'note'),
-                  ]
-         )
+        # IncludeLaunchDescription(PythonLaunchDescriptionSource(
+        #         get_package_share_directory('icp_obj_pose') + '/launch/component.launch.py'),
+        #         launch_arguments=[
+        #                    ('reference_ply_filepath', get_package_share_directory('nk_vision')+'/config/note.ply'),
+        #                    ('object_name', 'note'),
+        #           ]
+        #  )
     ])
